79dee72559274c155e93e908c1489136d214c071
lfneves <lfneves.dg@gmail.com>
2024-01-28 15:38:08 -0300

    build: add jacoco report path

--- src/test/kotlin/com/mvp/payment/mongodb/AutoConfigTest.kt
+++ src/test/kotlin/com/mvp/payment/mongodb/AutoConfigTest.kt
@@ -1,14 +1,14 @@
 package com.mvp.payment.mongodb
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
-import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit.jupiter.SpringExtension
+//import org.assertj.core.api.Assertions.assertThat
+//import org.junit.jupiter.api.Test
+//import org.junit.jupiter.api.extension.ExtendWith
+//import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+//import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+//import org.springframework.data.mongodb.core.MongoTemplate
+//import org.springframework.test.context.ActiveProfiles
+//import org.springframework.test.context.junit.jupiter.SpringExtension
 
 
 //@DataMongoTest
