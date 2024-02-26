60924a5ec42242113c24f0977fbe248ae93e6a3d
lfneves <lfneves.dg@gmail.com>
2024-02-26 00:07:42 -0300

    fix: add default application auth

---
+++ src/main/resources/database/init-mongo.js
@@ -0,0 +1,5 @@
+db.createCollection("tb_application_auth");
+db.tb_application_auth.insert({
+  "applicationId": "3e7869c2-add4-4fd5-b06c-0f11f3c34fa3",
+  "secret": "$2a$10$F5TLSdnNhChI89QS6txJWuEWqPkgndlLzOLEwoUREu8zGF1eQwMQ2"
+});
